d3bc4aefbda71bc098afd065b3bce2e489848cf8
Antipas Ben <benantipas@gmail.com>
2025-07-30 04:00:32 +0100

    added root pagetsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
+// src/app/page.tsx
+'use client';
+import Image from 'next/image';
+import LoginForm from '../components/LoginForm';
+
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <main className="relative w-full h-screen">
+      {/* Background Image */}
+      <Image
+        src="/bg.jpg"
+        alt="Background"
+        fill
+        className="object-cover w-full h-full -z-10"
+        priority
+      />
+
+      {/* Centered Card */}
+      <div className="flex items-center justify-center h-full">
+        <LoginForm />
+      </div>
+    </main>
+  );
 }
