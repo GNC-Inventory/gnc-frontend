ee72b7160aacbdb10e221b83eee82d323fc8cc12
Antipas Ben <benantipas@gmail.com>
2025-08-02 22:06:38 +0100

    renamed bk img

--- src/app/components/LoginPage.tsx
+++ src/app/components/LoginPage.tsx
@@ -18,7 +18,7 @@ export default function LoginPage() {
       <div 
         className="absolute inset-0 w-full h-full bg-cover bg-center bg-no-repeat"
         style={{
-          backgroundImage: "url('/background-image.jpg')", // Replace with your actual image path
+          backgroundImage: "url('/background-image.png')", 
         }}
       />
       
