d76e5f1f549885051c6e2ee8add4532bad117366
Antipas Ben <benantipas@gmail.com>
2025-08-06 23:07:36 +0100

    added Returnitems card

---
+++ src/app/components/dashboard/ReturnedItemsCard.tsx
@@ -0,0 +1,10 @@
+import StatsCard from './StatsCard';
+
+export default function ReturnedItemsCard() {
+  return (
+    <StatsCard 
+      title="Total returned items"
+      value="4"
+    />
+  );
+}
