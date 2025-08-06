697f1d465facaa15654b8adc9390503e60109729
Antipas Ben <benantipas@gmail.com>
2025-08-06 23:09:19 +0100

    added AverageReturned card

---
+++ src/app/components/dashboard/AmountReturnedCard.tsx
@@ -0,0 +1,10 @@
+import StatsCard from './StatsCard';
+
+export default function AmountReturnedCard() {
+  return (
+    <StatsCard 
+      title="Total amount returned"
+      value="₦ 20,000"
+    />
+  );
+}
