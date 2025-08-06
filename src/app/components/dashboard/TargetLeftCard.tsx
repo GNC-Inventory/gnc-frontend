ec182a1765aa244c91094e80370df8029ff039f4
Antipas Ben <benantipas@gmail.com>
2025-08-06 23:06:05 +0100

    added Taretleft card

---
+++ src/app/components/dashboard/TargetLeftCard.tsx
@@ -0,0 +1,10 @@
+import StatsCard from './StatsCard';
+
+export default function TargetLeftCard() {
+  return (
+    <StatsCard 
+      title="Target left"
+      value="₦ 15,000,000"
+    />
+  );
+}
