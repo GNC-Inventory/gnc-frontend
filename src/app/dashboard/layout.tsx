cb4c2c8e12dc79455322e5d68e6ebad67c21ea80
Antipas Ben <benantipas@gmail.com>
2025-08-06 21:54:15 +0100

    updated dashoard layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,4 +1,5 @@
 import Sidebar from '../components/Sidebar';
+import Navbar from '../components/Navbar';
 
 export default function DashboardLayout({
   children,
@@ -8,9 +9,15 @@ export default function DashboardLayout({
   return (
     <div className="flex h-screen bg-gray-50">
       <Sidebar />
-      <main className="flex-1 overflow-auto">
-        {children}
-      </main>
+      <div className="flex-1 flex flex-col overflow-hidden">
+        <Navbar 
+          title="Dashboard" 
+          subtitle="View your recent activities."
+        />
+        <main className="flex-1 overflow-auto">
+          {children}
+        </main>
+      </div>
     </div>
   );
 }
