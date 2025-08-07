eb61fe910f8834d26fed51c21d02dadd274938a6
Antipas Ben <benantipas@gmail.com>
2025-08-07 09:37:54 +0100

    added root page for products

---
+++ src/app/products/page.tsx
@@ -0,0 +1,9 @@
+export default function ProductsPage() {
+  return (
+    <div className="p-8">
+      <div className="text-gray-500">
+        Products page content coming soon...
+      </div>
+    </div>
+  );
+}
