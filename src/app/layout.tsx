cee7d8964fa0adf6e2a28fe90fbe8a5dc7a68ae2
Antipas Ben <benantipas@gmail.com>
2025-09-09 15:51:35 +0100

    updated global styling tff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,34 +17,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} ${geist.variable} antialiased`}>
-        {/* Side background images - fills white spaces on left and right of max-width container */}
-        <>
-          {/* Left side background */}
-          <div 
-            className="fixed left-0 top-0 w-full h-full bg-cover bg-center bg-no-repeat"
-            style={{
-              backgroundImage: 'url(/glob.jpg)',
-              clipPath: 'polygon(0 0, calc((100vw - 1440px) / 2) 0, calc((100vw - 1440px) / 2) 100%, 0 100%)',
-              zIndex: -1
-            }}
-          />
-          
-          {/* Right side background */}
-          <div 
-            className="fixed right-0 top-0 w-full h-full bg-cover bg-center bg-no-repeat"
-            style={{
-              backgroundImage: 'url(/glob.jpg)',
-              clipPath: 'polygon(calc(50vw + 720px) 0, 100% 0, 100% 100%, calc(50vw + 720px) 100%)',
-              zIndex: -1
-            }}
-          />
-        </>
-
+      <body 
+        className={`${inter.variable} ${geist.variable} antialiased`}
+        style={{
+          backgroundImage: 'url(/glob.jpg)',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          backgroundAttachment: 'fixed'
+        }}
+      >
         <div style={{ 
           maxWidth: '1440px', 
           margin: '0 auto',
-          backgroundColor: 'rgba(255, 255, 255, 0.95)',
           minHeight: '100vh',
           position: 'relative'
         }}>
