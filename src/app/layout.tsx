b56df41d54d972d00652bc2a501f1ef95597a128
Antipas Ben <benantipas@gmail.com>
2025-09-09 15:32:57 +0100

    updated global styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,29 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.variable} ${geist.variable} antialiased`}>
+        {/* Side background images - fills white spaces on left and right of max-width container */}
+        <>
+          {/* Left side background */}
+          <div 
+            className="fixed left-0 top-0 w-full h-full bg-cover bg-center bg-no-repeat"
+            style={{
+              backgroundImage: 'url(/glob.jpg)',
+              clipPath: 'polygon(0 0, calc((100vw - 1440px) / 2) 0, calc((100vw - 1440px) / 2) 100%, 0 100%)',
+              zIndex: -1
+            }}
+          />
+          
+          {/* Right side background */}
+          <div 
+            className="fixed right-0 top-0 w-full h-full bg-cover bg-center bg-no-repeat"
+            style={{
+              backgroundImage: 'url(/glob.jpg)',
+              clipPath: 'polygon(calc(50vw + 720px) 0, 100% 0, 100% 100%, calc(50vw + 720px) 100%)',
+              zIndex: -1
+            }}
+          />
+        </>
+
         <div style={{ maxWidth: '1440px', margin: '0 auto' }}>
           {children}
         </div>
